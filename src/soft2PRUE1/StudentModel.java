46c4254594b17aeabcddde36803804202d3ba1d6
Stefan Plavsic <stef.plav@gmail.com>
2018-03-18 23:14:20 +0100

    Table wird richig ausgegeben

--- src/soft2PRUE1/StudentModel.java
+++ src/soft2PRUE1/StudentModel.java
@@ -2,24 +2,25 @@ package soft2PRUE1;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.event.TableColumnModelEvent;
 import javax.swing.event.TableModelListener;
+import javax.swing.table.TableColumn;
 import javax.swing.table.TableModel;
 
 public class StudentModel implements TableModel {
 
-	public final static int COLCOUNT = 12;
-	/*
-	 * @JavaDoc
-	 * 
-	 */
+	private final static int COLCOUNT = 13;
+	private static  String[] colnames= {"ID","NAME","FIRSTNAME","SKZ","MAIL","UE1","UE2","UE3","UE4","UE5","UE5","SUM","GRADE"};
 
-	List<StudentGrades> list = new ArrayList<StudentGrades>();
-	HashSet<TableModelListener> listeners = new HashSet<TableModelListener>();
+	private List<StudentGrades> students = new ArrayList<StudentGrades>();
+	private HashSet<TableModelListener> listeners = new HashSet<TableModelListener>();
 
 	public StudentModel() {
-		list = new ArrayList<StudentGrades>();
+		students = new ArrayList<StudentGrades>();
+		TableColumn column= this.getColumn("ID"); 
 	}
 
 	@Override
@@ -35,15 +36,25 @@ public class StudentModel implements TableModel {
 	 */
 	@Override
 	public Class<?> getColumnClass(int index) {
-		// TODO Auto-generated method stub
+
 
 		if (checkIndex(index)) {
 
 			if (index <= 4) {
-				return list.get(0).getFirstName().getClass();
-			} else {
+				return new String().getClass();
+		
+			} 
+			 else if(index>4&&index<=11) {
 
-				return list.get(0).getPoints().getClass();
+				return students.get(0).getPoints().getClass();
+			}else if(index==12){
+				
+				return new Integer(12).getClass();
+				
+			}else{
+				
+				return null;//students.get(0).getGrade().getClass();
+				
 			}
 
 		}
@@ -58,13 +69,15 @@ public class StudentModel implements TableModel {
 
 	@Override
 	public String getColumnName(int columnIndex) {
-	
-	static	String[] colnames= {"ID","NAME","FIRSTNAME"};
-		
-		if (columnIndex<3) {
+		if (checkIndex(columnIndex)) {
 			return colnames[columnIndex];
+		
 		}
 		
+		return null;
+	
+		
+		/*
 		if (checkIndex(columnIndex)) {
 
 			switch (columnIndex) {
@@ -92,19 +105,19 @@ public class StudentModel implements TableModel {
 				return "UE6";
 			}
 		}
-		return null;
+		*/
+		
 	}
 
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return list.size();
+		return students.size();
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
 
-		StudentGrades st = list.get(rowIndex);
+		StudentGrades st = students.get(rowIndex);
 		if (checkIndex(columnIndex)) {
 
 			switch (columnIndex) {
@@ -118,18 +131,21 @@ public class StudentModel implements TableModel {
 				return st.getSkz();
 			case 4:
 				return st.getMail();
-			case 6:
+			case 5:
 				return st.getPoints()[0];
-			case 7:
+			case 6:
 				return st.getPoints()[1];
-			case 8:
+			case 7:
 				return st.getPoints()[2];
-			case 9:
+			case 8:
 				return st.getPoints()[3];
-			case 10:
+			case 9:
 				return st.getPoints()[4];
-			case 11:
-				return st.getPoints()[6];
+			case 10:
+				return st.getPoints()[5];
+				
+			case 11:return st.sumPoints;
+			case 12:return st.getGrade();
 
 			}
 		}
@@ -158,7 +174,7 @@ public class StudentModel implements TableModel {
 	@Override
 	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
 
-		StudentGrades st = list.get(rowIndex);
+		StudentGrades st = students.get(rowIndex);
 		if (checkIndex(columnIndex)) {
 
 			switch (columnIndex) {
@@ -201,15 +217,13 @@ public class StudentModel implements TableModel {
 	}
 	
 	public void add(StudentGrades st) {
-		
-		list.add(st);
-	
-		
-		
+		students.add(st);
+
 	}
 
+
 	private boolean checkIndex(int index) {
-		if (0 >= index && index <= 10) {
+		if (0 >= index || index <= COLCOUNT) {
 			return true;
 		}
 		return false;
